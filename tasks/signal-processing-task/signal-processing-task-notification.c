e55a525a2969bc0a191728d30aed5d11f172ee5d
Ozan Akyol <akyol856@ogr.eskisehir.edu.tr>
2025-01-04 12:43:51 +0300

    feat: change notification structure by replacing xQueue and xStreamBuffer

--- tasks/signal-processing-task/signal-processing-task-notification.c
+++ tasks/signal-processing-task/signal-processing-task-notification.c
@@ -1,8 +1,9 @@
 #include "signal-processing-task-notification.h"
 #include "FreeRTOS.h"
-#include "queue.h"
+#include "stream_buffer.h"
 #include "signal_processing.h"
 
+<<<<<<< HEAD
 #define SIGNAL_PROCESSING_TASK_QUEUE_LEN          (128)
 
 QueueHandle_t signal_processing_queue;
@@ -10,17 +11,28 @@ QueueHandle_t signal_processing_queue;
 bool init_signal_processing_task_notification(void){
     signal_processing_queue = xQueueCreate(SIGNAL_PROCESSING_TASK_QUEUE_LEN, sizeof(NotificationSignalProcessing_t));
     if(signal_processing_queue == NULL){
+=======
+StreamBufferHandle_t signal_processing_stream_buffer;
+
+bool init_signal_processing_task_notification(void){
+    signal_processing_stream_buffer = xStreamBufferCreate(sizeof(NotificationSignalProcessing_t), 1);
+    if(signal_processing_stream_buffer == NULL){
+>>>>>>> cf3cf2d (feat: change notification structure by replacing xQueue and xStreamBuffer)
         return false;
     }
     return true;
 }
 
 bool signal_processing_task_notify(NotificationSignalProcessing_t* ntf){
+<<<<<<< HEAD
     if(ntf == NULL || signal_processing_queue == NULL){
+=======
+    if(ntf == NULL || signal_processing_stream_buffer == NULL){
+>>>>>>> cf3cf2d (feat: change notification structure by replacing xQueue and xStreamBuffer)
         return false;
     }
 
-    if(xQueueSend(signal_processing_queue, (void*)ntf, 0) == pdPASS){
+    if(xStreamBufferSend(signal_processing_stream_buffer, (void*)ntf, sizeof(NotificationSignalProcessing_t), 0) == pdPASS){
         return true;
     }
 
@@ -30,11 +42,11 @@ bool signal_processing_task_notify(NotificationSignalProcessing_t* ntf){
 
 bool wait_signal_processing_task_notification(NotificationSignalProcessing_t* ntf){
     
-    if(ntf == NULL || signal_processing_queue == NULL){
+    if(ntf == NULL || signal_processing_stream_buffer == NULL){
         return false;
     }
 
-    if(xQueueReceive(signal_processing_queue, (void*)ntf, portMAX_DELAY) == pdPASS){
+    if(xStreamBufferReceive(signal_processing_stream_buffer, (void*)ntf, sizeof(NotificationSignalProcessing_t), portMAX_DELAY) == pdPASS){
         if(IS_VALID_SIGNAL_TASK_NOTIFICATION(ntf->id)){
             return true;
         }
--- tasks/speaker-task/speaker-task-notification.c
+++ tasks/speaker-task/speaker-task-notification.c
@@ -1,26 +1,30 @@
 #include "speaker-task-notification.h"
 #include "FreeRTOS.h"
-#include "queue.h"
+#include "stream_buffer.h"
 #include "speaker_task.h"
 
+<<<<<<< HEAD
 #define SPEAKER_TASK_QUEUE_LEN          (16)
 
 QueueHandle_t speaker_queue;
+=======
+StreamBufferHandle_t speaker_task_stream_buffer;
+>>>>>>> cf3cf2d (feat: change notification structure by replacing xQueue and xStreamBuffer)
 
 bool init_speaker_task_notification(void){
-    speaker_queue = xQueueCreate(SPEAKER_TASK_QUEUE_LEN, sizeof(NotificationSpeakerTask_t));
-    if(speaker_queue == NULL){
+    speaker_task_stream_buffer = xStreamBufferCreate(sizeof(NotificationSpeakerTask_t), 1);
+    if(speaker_task_stream_buffer == NULL){
         return false;
     }
     return true;
 }
 
 bool speaker_task_notify(NotificationSpeakerTask_t* ntf){
-    if(ntf == NULL || speaker_queue == NULL){
+    if(ntf == NULL || speaker_task_stream_buffer == NULL){
         return false;
     }
 
-    if(xQueueSend(speaker_queue, (void*)ntf, 0) == pdPASS){
+    if(xStreamBufferSend(speaker_task_stream_buffer, (void*)ntf, sizeof(NotificationSpeakerTask_t), 0) == pdPASS){
         return true;
     }
 
@@ -30,11 +34,11 @@ bool speaker_task_notify(NotificationSpeakerTask_t* ntf){
 
 bool wait_speaker_task_notification(NotificationSpeakerTask_t* ntf){
     
-    if(ntf == NULL || speaker_queue == NULL){
+    if(ntf == NULL || speaker_task_stream_buffer == NULL){
         return false;
     }
 
-    if(xQueueReceive(speaker_queue, (void*)ntf, portMAX_DELAY) == pdPASS){
+    if(xStreamBufferReceive(speaker_task_stream_buffer, (void*)ntf, sizeof(NotificationSpeakerTask_t), portMAX_DELAY) == pdPASS){
         if(IS_VALID_SPEAKER_TASK_NOTIFICATION(ntf->id)){
             return true;
         }
